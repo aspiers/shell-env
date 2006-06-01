906f0db230e3101df0b18401348ec8a8f4bc6b67
adam <adam>
2006-06-01 15:02:03 +0000

    fix untar of tar.bz2

--- bin/lesspipe.sh
+++ bin/lesspipe.sh
@@ -21,8 +21,8 @@ lesspipe() {
 		$DECOMPRESSOR -- "$1"
 	fi ;;
   *.tar) tar tvvf "$1" ;;
-  *.tgz|*.tar.gz|*.tar.[zZ]) tar tzvvf "$1" ;;
-  *.tar.bz2|*.tbz2) tar tIvvf "$1" ;;
+  *.tgz|*.tar.gz|*.tar.[zZ]) tar ztvvf "$1" ;;
+  *.tar.bz2|*.tbz2) tar jtvvf "$1" ;;
   *.[zZ]|*.gz) gzip -dc -- "$1" ;;
   *.bz2) bzip2 -dc -- "$1" ;;
   *.zip) zipinfo -- "$1" ;;
