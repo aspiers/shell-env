b141f1a9707e472d98c2973a26d69ba9a162bee4
Adam Spiers <dotfiles@adamspiers.org>
2012-02-23 10:57:03 +0000

    add xz

--- bin/lesspipe.sh
+++ bin/lesspipe.sh
@@ -39,6 +39,9 @@ lesspipe() {
     *.bz2)
       bzip2 -dc -- "$1"
       ;;
+    *.xz)
+      xz -dc -- "$1"
+      ;;
     *.zip|*.Zip|*.ZIP|*.jar|*.war|*.sar|*.xpi|*.job|*.pylib)
       zipinfo -- "$1"
       ;;
